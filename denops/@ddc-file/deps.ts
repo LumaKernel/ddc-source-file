93518518c3db8c33504d8848d85ea1f50d0d6fe9
Luma <world@luma.email>
2021-10-17 00:39:18 +0900

    chore: update deno deps

--- denops/@ddc-file/deps.ts
+++ denops/@ddc-file/deps.ts
@@ -1,17 +1,17 @@
 export type {
   Candidate,
   Context,
-} from "https://deno.land/x/ddc_vim@v0.14.0/types.ts";
-export { BaseSource } from "https://deno.land/x/ddc_vim@v0.14.0/types.ts";
-export type { Denops } from "https://deno.land/x/ddc_vim@v0.14.0/deps.ts";
-export { fn, vars } from "https://deno.land/x/ddc_vim@v0.14.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v0.17.0/types.ts";
+export { BaseSource } from "https://deno.land/x/ddc_vim@v0.17.0/types.ts";
+export type { Denops } from "https://deno.land/x/ddc_vim@v0.17.0/deps.ts";
+export { fn, vars } from "https://deno.land/x/ddc_vim@v0.17.0/deps.ts";
 export type {
   GatherCandidatesArguments,
-} from "https://deno.land/x/ddc_vim@v0.14.0/base/source.ts";
-export * as path from "https://deno.land/std@0.108.0/path/mod.ts";
-export * as io from "https://deno.land/std@0.108.0/io/mod.ts";
-export * as fs from "https://deno.land/std@0.108.0/fs/mod.ts";
-export * as asserts from "https://deno.land/std@0.108.0/testing/asserts.ts";
+} from "https://deno.land/x/ddc_vim@v0.17.0/base/source.ts";
+export * as path from "https://deno.land/std@0.111.0/path/mod.ts";
+export * as io from "https://deno.land/std@0.111.0/io/mod.ts";
+export * as fs from "https://deno.land/std@0.111.0/fs/mod.ts";
+export * as asserts from "https://deno.land/std@0.111.0/testing/asserts.ts";
 export {
   asyncIteratorFrom as fromA,
   iteratorFrom as from,
