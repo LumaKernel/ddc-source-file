cd01561dade84840a3f05311709945176e3cccab
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-11-10 09:57:58 +0900

    Update deps

--- denops/@ddc-file/deps.ts
+++ denops/@ddc-file/deps.ts
@@ -1,22 +1,23 @@
 export type {
   Context,
   Item,
-} from "https://deno.land/x/ddc_vim@v3.1.0/types.ts";
-export { BaseSource } from "https://deno.land/x/ddc_vim@v3.1.0/types.ts";
-export type { Denops } from "https://deno.land/x/ddc_vim@v3.1.0/deps.ts";
-export { fn, vars } from "https://deno.land/x/ddc_vim@v3.1.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
+export { BaseSource } from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
+export type { Denops } from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
+export { fn, vars } from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
 export type {
   GatherArguments,
-} from "https://deno.land/x/ddc_vim@v3.1.0/base/source.ts";
-export * as path from "https://deno.land/std@0.162.0/path/mod.ts";
-export * as io from "https://deno.land/std@0.162.0/io/mod.ts";
-export * as fs from "https://deno.land/std@0.162.0/fs/mod.ts";
-export * as asserts from "https://deno.land/std@0.162.0/testing/asserts.ts";
+  GetCompletePositionArguments,
+} from "https://deno.land/x/ddc_vim@v4.1.0/base/source.ts";
+export * as path from "https://deno.land/std@0.205.0/path/mod.ts";
+export * as io from "https://deno.land/std@0.205.0/io/mod.ts";
+export * as fs from "https://deno.land/std@0.205.0/fs/mod.ts";
+export * as asserts from "https://deno.land/std@0.205.0/testing/asserts.ts";
 export {
   asyncIteratorFrom as fromA,
   iteratorFrom as from,
   wrapAsyncIterator as wrapA,
   wrapIterator as wrap,
 } from "https://deno.land/x/iterator_helpers@v0.1.2/mod.ts";
-import homeDir from "https://deno.land/x/dir@1.5.1/home_dir/mod.ts";
+import homeDir from "https://deno.land/x/dir@1.5.2/home_dir/mod.ts";
 export { homeDir };
