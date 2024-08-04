896eba5386e5a7509a898ec6bbd6a57ea0881e63
Luma <world@luma.email>
2024-08-05 03:44:45 +0900

    Update deps.ts

--- denops/@ddc-file/deps.ts
+++ denops/@ddc-file/deps.ts
@@ -1,19 +1,19 @@
-export type { Context, Item } from "jsr:@shougo/ddc-vim@~6.0.0/types";
-export { BaseSource } from "jsr:@shougo/ddc-vim@~6.0.0/types";
+export type { Context, Item } from "jsr:@shougo/ddc-vim@^6.0.0/types";
+export { BaseSource } from "jsr:@shougo/ddc-vim@^6.0.0/types";
 export type {
   GatherArguments,
   GetCompletePositionArguments,
-} from "jsr:@shougo/ddc-vim@~6.0.0/source";
+} from "jsr:@shougo/ddc-vim@^6.0.0/source";
 
-export type { Denops } from "jsr:@denops/std@~7.0.1";
-export * as fn from "jsr:@denops/std@~7.0.1/function";
-export * as vars from "jsr:@denops/std@~7.0.1/variable";
+export type { Denops } from "jsr:@denops/std@^7.0.1";
+export * as fn from "jsr:@denops/std@^7.0.1/function";
+export * as vars from "jsr:@denops/std@^7.0.1/variable";
 
-export * as path from "jsr:@std/path@~1.0.2";
-export * as fs from "jsr:@std/fs@~1.0.0";
-export * as asserts from "jsr:@std/assert@~1.0.1";
-export * as posix from "jsr:@std/path@~1.0.2/posix";
-export * as windows from "jsr:@std/path@~1.0.2/windows";
+export * as path from "jsr:@std/path@^1.0.2";
+export * as fs from "jsr:@std/fs@^1.0.0";
+export * as asserts from "jsr:@std/assert@^1.0.1";
+export * as posix from "jsr:@std/path@^1.0.2/posix";
+export * as windows from "jsr:@std/path@^1.0.2/windows";
 
 export {
   asyncIteratorFrom as fromA,
